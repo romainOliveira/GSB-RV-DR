156ea8681434f5a9afa35717c98764b8a59fca9d
romainOliveira <developpeur@slam.sio.lla.fr>
2020-03-03 14:41:48 +0100

    third commit

--- src/fr/gsb/rv/dr/GSBRVDR.java
+++ src/fr/gsb/rv/dr/GSBRVDR.java
@@ -5,13 +5,24 @@
  */
 package fr.gsb.rv.dr;
 
+import java.util.Optional;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 
 /**
  *
@@ -19,24 +30,80 @@ import javafx.stage.Stage;
  */
 public class GSBRVDR extends Application {
     
+    boolean session = false;
+    
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
+        SeparatorMenuItem separatorQuitter = new SeparatorMenuItem();
+        MenuBar barreMenus = new MenuBar();
+        Menu menuFichier = new Menu("Fichier");
+        Menu menuRapports = new Menu("Rapports");
+        Menu menuPraticiens = new Menu("Praticiens");
+        MenuItem itemSeConnecter = new MenuItem("Se connecter");
+        MenuItem itemSeDeconnecter = new MenuItem("Se déconnecter");
+        MenuItem itemQuitter = new MenuItem("Quitter");
+        MenuItem itemConsulter = new MenuItem("Consulter");
+        MenuItem itemHesitant = new MenuItem("Hésitant");
+        ButtonType btnOui = new ButtonType("Oui");
+        ButtonType btnNon = new ButtonType("Non");        
+        BorderPane bp = new BorderPane();
+        menuFichier.getItems().add(itemSeConnecter);
+        menuFichier.getItems().add(itemSeDeconnecter);
+        itemSeDeconnecter.setDisable(!session);
+        menuFichier.getItems().add(separatorQuitter);
+        menuFichier.getItems().add(itemQuitter);
+        barreMenus.getMenus().add(menuFichier);
+        menuRapports.getItems().add(itemConsulter);
+        barreMenus.getMenus().add(menuRapports);
+        menuRapports.setDisable(!session);
+        menuPraticiens.getItems().add(itemHesitant);
+        barreMenus.getMenus().add(menuPraticiens);
+        menuPraticiens.setDisable(!session);
+        itemQuitter.setAccelerator(new KeyCodeCombination(KeyCode.X, KeyCombination.CONTROL_DOWN));
+         
+        
+        itemSeConnecter.setOnAction((ActionEvent event) -> {
+            session = true;
+            itemSeDeconnecter.setDisable(!session);
+            itemSeConnecter.setDisable(session);
+            menuRapports.setDisable(!session);
+            menuPraticiens.setDisable(!session);
+            bp.setCenter(new Label("Se connecter"));
         });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        itemSeDeconnecter.setOnAction((ActionEvent event) -> {
+            session = false;
+            itemSeDeconnecter.setDisable(!session);
+            itemSeConnecter.setDisable(session);
+            menuRapports.setDisable(!session);
+            menuPraticiens.setDisable(!session);
+            bp.setCenter(new Label("Se déconnecter"));
+        });
         
-        Scene scene = new Scene(root, 300, 250);
+        itemQuitter.setOnAction((ActionEvent event) -> {
+            Alert alertQuitter = new Alert( Alert.AlertType.CONFIRMATION ) ;
+                alertQuitter.setTitle( "Quitter" );
+                alertQuitter.setHeaderText( "Demande de confirmation" );
+                alertQuitter.setContentText( "Voulez-vous quitter l'application ?" );
+                alertQuitter.getButtonTypes().setAll(btnOui, btnNon);
+                Optional<ButtonType> result = alertQuitter.showAndWait();
+                     if (result.get() == btnOui) {
+                         Platform.exit();
+                        System.out.println(session);
+                     }
+        });
         
-        primaryStage.setTitle("Hello World!");
+        itemConsulter.setOnAction((ActionEvent event) -> {
+            bp.setCenter(new Label("Consulter"));
+        });
+        
+        itemHesitant.setOnAction((ActionEvent event) -> {
+            bp.setCenter(new Label("Hésitant"));
+        });
+        
+        bp.setTop(barreMenus);
+        Scene scene = new Scene(bp, 500, 250);
+        primaryStage.setTitle("GSB-RV-DR");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
